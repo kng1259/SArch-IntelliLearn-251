03cb005335f1e1fa4084d2db31cf144a360a4a49
bluez44 <vlqvinh444@gmail.com>
2025-11-30 00:21:40 +0700

    student dashboard, add auth context, student course detail

--- web-app/next.config.ts
+++ web-app/next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
+      },
+    ],
+  },
 };
+
+// https://images.unsplash.com/photo-1633356122544-f134324a6cee?w=800
 
 export default nextConfig;
