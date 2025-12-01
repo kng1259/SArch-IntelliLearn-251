432a21f8d8b722304976c0dffb59e7e60be20359
bluez44 <vlqvinh444@gmail.com>
2025-12-01 14:57:00 +0700

    Revert "Merge branch 'frontend' into develop"
    This reverts commit 5bf168a59faff7454fe5618d51e520d4f45f2b2e, reversing
    changes made to f1d1a0a0744a566864ab43e8447f284e5a2d869c.

--- web-app/app/page.tsx
+++ web-app/app/page.tsx
@@ -1,23 +1,5 @@
-"use client";
-
-import { useAuth } from "@/context/AuthContext";
-import { useRouter } from "next/navigation";
+import { redirect } from 'next/navigation';
 
 export default function Home() {
-  const authContext = useAuth();
-  const router = useRouter();
-
-  console.log("Current user:", authContext);
-
-  if (!authContext.user) {
-    router.push("/signin");
-  }
-
-  if (authContext.user?.role === "student") {
-    router.push("/student/dashboard");
-  }
-
-  if (authContext.user?.role === "tutor") {
-    router.push("/tutor/dashboard");
-  }
+  redirect('/signin');
 }
--- web-app/next.config.ts
+++ web-app/next.config.ts
@@ -3,17 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'standalone',
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        pathname: "/**",
-      },
-    ],
-  },
 };
-
-// https://images.unsplash.com/photo-1633356122544-f134324a6cee?w=800
 
 export default nextConfig;
